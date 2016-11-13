d0d0ebc45a55559bf3ccf8ce0a09f61e051b9b10
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-11-13 20:18:15 +0100

    refactors directive matching styleguide

---
+++ iteration-1/src/app/rating/rating.directive.js
@@ -0,0 +1,82 @@
+(function () {
+  angular.module('sui-rating').directive('suiRating', directive);
+
+  function directive() {
+    return {
+      restrict: 'E',
+      template: '<div class="ui rating star">' +
+                  '<i id="{{vm.id + 1}}" class="icon" ng-click="vm.clicked(1)" ng-mouseenter="vm.mouseEntered(1)" ng-mouseleave="vm.mouseLeft(1)"></i>' +
+                  '<i id="{{vm.id + 2}}" class="icon" ng-click="vm.clicked(2)" ng-mouseenter="vm.mouseEntered(2)" ng-mouseleave="vm.mouseLeft(2)"></i>' +
+                  '<i id="{{vm.id + 3}}" class="icon" ng-click="vm.clicked(3)" ng-mouseenter="vm.mouseEntered(3)" ng-mouseleave="vm.mouseLeft(3)"></i>' +
+                  '<i id="{{vm.id + 4}}" class="icon" ng-click="vm.clicked(4)" ng-mouseenter="vm.mouseEntered(4)" ng-mouseleave="vm.mouseLeft(4)"></i>' +
+                  '<i id="{{vm.id + 5}}" class="icon" ng-click="vm.clicked(5)" ng-mouseenter="vm.mouseEntered(5)" ng-mouseleave="vm.mouseLeft(5)"></i>' +
+                '</div>',
+      bindToController: {
+        id: '@',
+        size: '@',
+        type: '@',
+        model: '=ngModel'
+      },
+      controllerAs: 'vm',
+      controller: ctrl
+    };
+  }
+})();
+
+ctrl.$inject = ['$scope'];
+function ctrl($scope){
+  var vm = this;
+  vm.current = 0;        
+  vm.init = init;
+  vm.mouseEntered = mouseEntered;
+  vm.mouseLeft = mouseLeft;
+  vm.clicked = clicked;
+  
+  function init() {
+    if (!vm.model) { 
+      vm.model = 0;
+    }
+
+    if (vm.model < 1 && vm.model > 5) {
+      vm.model = 0;
+    }
+
+    $scope.$watch('model', function (val) {
+      vm.clicked(val);
+    });
+  }
+
+
+  function mouseEntered (iconIndex) {
+    for (var i = 1; i <= iconIndex; i++) {
+      document.getElementById(vm.id + i).className = 'icon active';
+    }
+  };
+
+  function mouseLeft(iconIndex) {
+    var firstNotRatedSymbol = vm.current + 1;
+
+    for (var i = 1; i <= vm.current; i++) {
+      document.getElementById(vm.id + i).className = 'icon active';
+    }
+
+    for (var i = firstNotRatedSymbol; i <= 5; i++) {
+      document.getElementById(vm.id + i).className = 'icon';
+    }
+  };
+
+  function clicked(iconIndex) {
+    for (var i = 1; i <= iconIndex; i++) {
+      document.getElementById(vm.id + i).className = 'icon active';
+    }
+
+    if (iconIndex) {
+      vm.current = iconIndex;
+    } else {
+      vm.current = 0;
+    }
+  };
+
+  init();
+
+}
